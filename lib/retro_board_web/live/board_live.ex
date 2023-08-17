ce1a40f1766992bd769d044b51fd8929ea8fa244
jexi-The Linux <vnjeru85@gmail.com>
2023-08-17 23:33:50 +0300

    list our boards

--- lib/retro_board_web/live/board_live.ex
+++ lib/retro_board_web/live/board_live.ex
@@ -1,5 +1,14 @@
 defmodule RetroBoardWeb.BoardLive do
+  alias RetroBoard.Boards
   use RetroBoardWeb, :live_view
 
-
+  @impl true
+  def mount(_params, _session, socket) do
+    {
+      :ok,
+      socket
+      |> assign(:boards, Boards.list_boards())
+      |> assign(:page_title, "Our Boards")
+    }
+  end
 end
